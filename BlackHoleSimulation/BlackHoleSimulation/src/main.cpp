c6ef0f0efa86743788195aaa6bcd2582c468dc25
Michal Becmer <93706431+Clouddeboi@users.noreply.github.com>
2025-08-11 00:58:39 +0100

    🚧 Commented main file in detail

--- BlackHoleSimulation/BlackHoleSimulation/src/main.cpp
+++ BlackHoleSimulation/BlackHoleSimulation/src/main.cpp
@@ -4,14 +4,19 @@
 #include <iostream>
 
 int main() {
+    //Initialize GLFW
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
 
+    //Telling GLFW which version of OpenGl we will use
+    //We will use 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
+    //Creating a window titled "Black Hole Simulation"
+    //And 800x600 pixels by size
     GLFWwindow* window = glfwCreateWindow(800, 600, "Black Hole Simulation", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -19,20 +24,33 @@ int main() {
         return -1;
     }
 
+    //Make this the current window in OpenGL
     glfwMakeContextCurrent(window);
+
+    //Loading all OpenGl funtion pointers (using GLAD)
+    //Must happen after making the current context
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cerr << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
+    //Main loop that keeps running until we close the window
     while (!glfwWindowShouldClose(window)) {
+        //Clears the screen with the current clear color
         glClear(GL_COLOR_BUFFER_BIT);
 
+        //Swap the front and back buffers
+        //(Double Buffering)
         glfwSwapBuffers(window);
+
+        //Process any input or events
         glfwPollEvents();
     }
 
+    //Cleanup
     glfwDestroyWindow(window);
     glfwTerminate();
+
+    //End of program
     return 0;
 }
