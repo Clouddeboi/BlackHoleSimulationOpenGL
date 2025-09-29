27780ac2b90cad48a9f9af678bceb6396100fbaa
Michal Becmer <93706431+Clouddeboi@users.noreply.github.com>
2025-09-29 01:01:48 +0100

    🚧 Updated project settings for release
    Added a try catch to main to identify build issues

--- BlackHoleSimulation/BlackHoleSimulation/src/main.cpp
+++ BlackHoleSimulation/BlackHoleSimulation/src/main.cpp
@@ -4,9 +4,16 @@
 
 #include "../headers/app.hpp"
 #include "../headers/renderer.hpp"
+#include <iostream>
 
 int main() {
-    App app(1280, 720, "Black Hole Simulation");
-    app.run();
+    try {
+        App app(1280, 720, "Black Hole Simulation");
+        app.run();
+    }
+    catch (const std::exception& ex) {
+        std::cerr << "Exception: " << ex.what() << std::endl;
+        system("pause");
+    }
     return 0;
 }
